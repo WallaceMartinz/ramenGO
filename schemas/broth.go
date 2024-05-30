5a77fba9fb41e176d746d43dfc95d0653c30d53d
WallaceMartinz <wallace.mota@sptech.school>
2024-05-30 19:09:59 -0300

    feat: add mocks

--- schemas/broth.go
+++ schemas/broth.go
@@ -8,3 +8,32 @@ type Broth struct {
 	Description   string  `json:"description"`
 	Price         float64 `json:"price"`
 }
+
+var (
+	broths = []Broth{
+		{
+			ID: 			1,
+			ImageInactive: "https://tech.redventures.com.br/icons/salt/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/salt/active.svg",
+			Name: 		   "Salt",
+			Description:   "Simple like the seawater, nothing more",
+			Price: 		    10,
+		},
+		{
+			ID: 			2,
+			ImageInactive: "https://tech.redventures.com.br/icons/shoyu/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/shoyu/active.svg",
+			Name: 		   "Shoyu",
+			Description:   "The good old and traditional soy sauce",
+			Price: 		    10,
+		},
+		{
+			ID: 		    3,
+			ImageInactive: "https://tech.redventures.com.br/icons/miso/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/miso/active.svg",
+			Name: 		   "Miso",
+			Description:   "Paste made of fermented soybeans",
+			Price: 			12,
+		},
+	}
+)
--- schemas/protein.go
+++ schemas/protein.go
@@ -8,3 +8,32 @@ type Protein struct {
 	Description   string  `json:"description"`
 	Price         float64 `json:"price"`
 }
+
+var (
+	proteins = []Protein{
+		{
+			ID: 			1,
+			ImageInactive: "https://tech.redventures.com.br/icons/pork/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/pork/active.svg",
+			Name: 		   "Chasu",
+			Description:   "A sliced flavourful pork meat with a selection of season vegetables.",
+			Price: 		    10,
+		},
+		{
+			ID: 			2,
+			ImageInactive: "https://tech.redventures.com.br/icons/yasai/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/yasai/active.svg",
+			Name: 		   "Yasai Vegetarian",
+			Description:   "A delicious vegetarian lamen with a selection of season vegetables.",
+			Price: 		    10,
+		},
+		{
+			ID: 		    3,
+			ImageInactive: "https://tech.redventures.com.br/icons/chicken/inactive.svg",
+			ImageActive:   "https://tech.redventures.com.br/icons/chicken/active.svg",
+			Name: 		   "Karaague",
+			Description:   "Three units of fried chicken, moyashi, ajitama egg and other vegetables.",
+			Price: 			12,
+		},
+	}
+)
