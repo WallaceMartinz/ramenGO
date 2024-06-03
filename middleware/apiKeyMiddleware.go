c4d0592b8f09067c4e7210c1e13bf50e550585b8
WallaceMartinz <wallace.mota@sptech.school>
2024-06-03 12:19:49 -0300

    feat: validate x-api-key

--- middleware/apiKeyMiddleware.go
+++ middleware/apiKeyMiddleware.go
@@ -1,18 +1,32 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/WallaceMartinz/ramenGO/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 // ApiKeyMiddleware is a function to validate the presence of an API key in the request headers.
 func ApiKeyMiddleware() gin.HandlerFunc {
-    return func(ctx *gin.Context) {
-        apikey := ctx.GetHeader("x-api-key")
-        if apikey == "" {
-            handlers.HandleMissingAPIKey(ctx)
-            return
-        }
-        ctx.Next()
-    }
+	return func(ctx *gin.Context) {
+		apikeyFromHeader := ctx.GetHeader("x-api-key")
+		if apikeyFromHeader == "" {
+			handlers.HandleMissingAPIKey(ctx)
+			return
+		}
+
+		apikeyFromEnv := os.Getenv("X_API_KEY")
+		if apikeyFromEnv == "" {
+			handlers.HandleMissingAPIKey(ctx)
+			return
+		}
+
+		if apikeyFromEnv != apikeyFromHeader {
+			handlers.HandleMissingAPIKey(ctx)
+			return
+		}
+
+		ctx.Next()
+	}
 }
